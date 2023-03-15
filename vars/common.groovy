bf11a69b0d766acd3a83e50d21c12d02b69d2e3f
Radhika <radhika.b4329@gmail.com>
2023-03-15 18:47:08 +0530

    shipping

--- vars/common.groovy
+++ vars/common.groovy
@@ -6,4 +6,16 @@ def compile() {
   if (app_lang == "maven") {
     sh 'mvn package'
   }
+
+}
+
+def unittests() {
+
+  if (app_lang == "nodejs") {
+//    sh 'npm test'
+    sh 'echo Test Cases'
+  }
+  if (app_lang == "maven") {
+    sh 'mvn test'
+  }
 }
