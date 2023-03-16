c240c71d09f68f8983c70a373c08b7cc61dd2d00
Radhika <radhika.b4329@gmail.com>
2023-03-16 18:24:52 +0530

    cart

--- vars/common.groovy
+++ vars/common.groovy
@@ -27,5 +27,5 @@ def unittests() {
 }
 
 def email(email_note) {
-  mail bcc: '', body: 'Job Failed : ', cc: '', from: 'radhika.bhagavatham@gmail.com', replyTo: '', subject: 'TEST FROM JENKINS', to: 'radhika.bhagavatham@gmail.com'
+  mail bcc: '', body: "Job Failed - ${JOB_BASE_NAME}\nJenkins URL - ${JOB_URL}", cc: '', from: 'radhika.bhagavatham@gmail.com', replyTo: '', subject: 'TEST FROM JENKINS', to: 'radhika.bhagavatham@gmail.com'
 }
