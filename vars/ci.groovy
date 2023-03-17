06a66181c3107f68d1a4544e4844491019731a46
Radhika <radhika.b4329@gmail.com>
2023-03-17 16:32:37 +0530

    cart

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -31,7 +31,7 @@ def call() {
           }
           steps {
 
-            sh "sonar-scanner -Dsonar.host.url=http://3.235.163.149:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+            sh "sonar-scanner -Dsonar.host.url=http://3.239.14.91:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
 
           }
         }
