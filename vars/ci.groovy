13a2ae5ed2fb7563b72ecc1f2755d76192253f82
Radhika <radhika.b4329@gmail.com>
2023-03-27 22:00:34 +0530

    cart

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,4 +1,5 @@
 def call() {
+
   if(!env.SONAR_EXTRA_OPTS) {
     env.SONAR_EXTRA_OPTS = " "
   }
@@ -23,7 +24,7 @@ def call() {
         SONAR_PASS = sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
         SONAR_USER = sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-          sh "sonar-scanner -Dsonar.host.url=http://54.237.156.156:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+          sh "sonar-scanner -Dsonar.host.url=http://34.234.225.46:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
         }
       }
       stage('Upload code to centralized place') {
