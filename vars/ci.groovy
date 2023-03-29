d6f979e8539264d17a2fc23c65a714bd5aa1f244
Radhika <radhika.b4329@gmail.com>
2023-03-29 19:46:00 +0530

    test

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -3,6 +3,13 @@ def call() {
   if(!env.SONAR_EXTRA_OPTS) {
     env.SONAR_EXTRA_OPTS = " "
   }
+
+  if(!env.TAG_NAME) {
+    env.PUSH_CODE = "false"
+  } else {
+    env.PUSH_CODE = "true"
+  }
+
   try {
     node('workstation') {
 
@@ -30,8 +37,13 @@ def call() {
 
         }
       }
-      stage('Upload code to centralized place') {
-        echo 'Upload'
+
+      if(env.PUSH_CODE == "true") {
+        stage('Upload code to centralized place') {
+          echo 'Upload'
+
+        }
+
       }
 
 
