a720d37349c94dec83ccca9fe4299b0c7e077aff
Radhika <radhika.b4329@gmail.com>
2023-05-08 23:40:43 +0530

    update roboshop-infra

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -22,6 +22,9 @@ def call() {
                     sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
                 }
             }
+            stage('Terraform Apply') {
+                steps {
+                    sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
 
         }
 
